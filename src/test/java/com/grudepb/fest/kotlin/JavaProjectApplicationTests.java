1f1c7e911728e58b4860a439be4e0e2ffa8d1ec9
Bruno N. Melo <brunonobrega.melo@gmail.com>
2019-11-27 21:35:27 -0300

    commit inicial

---
+++ src/test/java/com/grudepb/fest/kotlin/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grudepb.fest.kotlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
